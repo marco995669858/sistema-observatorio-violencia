76b37d42d615aa31d62646f977973c70d578a1ef
marco995669858 <marco995669858@gmail.com>
2026-04-10 22:04:17 -0500

    Fy: Implementación de funcionalidades

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { Dashboard } from './features/dashboard/dashboard';
 import { Alertas } from './features/alerts/alertas/alertas';
 import { ReporteAlerta } from './features/alerts/reporte-alerta/reporte-alerta';
 import { Login } from './features/auth/login/login';
-import { Register } from './features/auth/register/register';
 import { MapaRiesgo } from './features/mapa-riesgo/mapa-riesgo';
 import { MapaAnalisisRiesgo } from './features/mapa-analisis-riesgo/mapa-analisis-riesgo';
 import { authGuard } from './core/guards/auth-guard';
